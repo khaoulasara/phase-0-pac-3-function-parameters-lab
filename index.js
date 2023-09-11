f45fd3f4ad9661a298047f090dc52f019ac54e66
vboxuser <khaoulaaourrath@gmail.com>
2023-09-11 22:01:09 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function introduction(name) {
   function introductionWithLanguageOptional(name, language = "JavaScript") {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
+  const greeting = introduction('John');
+console.log(greeting); 
+
+const greetingWithLanguage = introductionWithLanguage('Alice', 'Python');
+console.log(greetingWithLanguage); 
+
+const defaultGreeting = introductionWithLanguageOptional('Bob');
+console.log(defaultGreeting); 
